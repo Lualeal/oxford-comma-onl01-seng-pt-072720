8f9a206c0c29612e96069a3d8bc31c885e01ce09
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 15:30:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,3 @@
- # def oxford_comma(array)
-    #array.join(" and ")
-  #end
-  
-#  def oxford_comma(array)
-    #case array.size 
-    #when 2 
-    #  array.join(" and")
-   # when 3
-     # array.join(" , "), array.last(" and ")
-  #  else 
-      #[array[0..-3].join(', '), arr.last].join(', and ')
- #   end
- # end 
-
 
 #def oxford_comma(array)
  #   if array.length == 2
