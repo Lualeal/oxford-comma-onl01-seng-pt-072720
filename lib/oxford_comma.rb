99ffee8650868dc185fbbf7cfd697f8f519c5a12
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 15:29:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,16 +14,16 @@
  # end 
 
 
-def oxford_comma(array)
-    if array.length == 2
-        array.join(" and ")
-    elsif array.length > 2
-        array.insert(-2, "and")
-        array[0..-1].join(", ")
-    else
-        array.join
-    end
-end
+#def oxford_comma(array)
+ #   if array.length == 2
+  #      array.join(" and ")
+   # elsif array.length > 2
+    #    array.insert(-2, "and")
+     #   array[0..-1].join(", ")
+  #  else
+   #     array.join
+  #  end
+#end
 
 
 def oxford_comma(array)
